7d2a501db6e529e7aafbbf2088e928cacd9060b7
Kovas Boguta <kovas.boguta@gmail.com>
2013-08-05 16:41:26 -0700

    initial commit

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject paredit-widget "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[seesaw "1.4.4-SNAPSHOT"]
+                 [org.kovas/paredit.clj "0.20.1-SNAPSHOT"]
+                 ])
---
+++ test/paredit_widget/core_test.clj
@@ -0,0 +1,7 @@
+(ns paredit-widget.core-test
+  (:require [clojure.test :refer :all]
+            [paredit-widget.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
